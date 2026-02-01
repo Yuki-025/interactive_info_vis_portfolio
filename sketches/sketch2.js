bbe2032e34f790c4490e3de507e2284a1a8b9dab
Yuki-025 <zha12935@uw.edu>
2026-02-01 11:29:20 -0800

    clear placeholder sketch to prepare breathing wave clock

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -4,11 +4,7 @@ registerSketch('sk2', function (p) {
     p.createCanvas(p.windowWidth, p.windowHeight);
   };
   p.draw = function () {
-    p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    p.background(240);
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
